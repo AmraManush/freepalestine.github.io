ca7fbadd4e2c4b3f33979a7b9050208ab9d407b1
Md Ariful Islam <arifulatwork@gmail.com>
2025-04-07 23:25:57 +0300

    Update my static

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // Change from adapter-auto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',  // Output directory (default: 'build')
+      assets: 'build',
+      fallback: null,   // Enable SPA mode (optional)
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/my-boycott', // Replace with your repo name
+    },
+  },
 };
 
 export default config;
